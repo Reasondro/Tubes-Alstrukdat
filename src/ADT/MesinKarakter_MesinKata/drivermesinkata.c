f64336d238749ecb84834570986d69fb94884e25
Reasondro <alessandrohasian16@gmail.com>
2023-11-09 09:23:14 +0700

    add WordToString

--- src/ADT/MesinKarakter_MesinKata/drivermesinkata.c
+++ src/ADT/MesinKarakter_MesinKata/drivermesinkata.c
@@ -14,18 +14,26 @@ int main()
     char original[] = "halo pak riza\nhehehe";
     char copy[50];
 
-    stringCopy(copy, original); // contoh copy string ke variable lain
-    printf("Original: %s\n", original);
-    printf("Copy: %s\n", copy);
+    // stringCopy(copy, original); // contoh copy string ke variable lain
+    // printf("Original: %s\n", original);
+    // printf("Copy: %s\n", copy);
 
-    if (IsSameString(original, copy)) // contoh compare string
-    {
-        printf("Sama\n");
-    }
-    else
-    {
-        printf("Tidak sama\n");
-    }
+    // if (IsSameString(original, copy)) // contoh compare string
+    // {
+    //     printf("Sama\n");
+    // }
+    // else
+    // {
+    //     printf("Tidak sama\n");
+    // }
+    char penyanyi[50];                    // ini misal string yang kalian mo isi
+    printf("Masukkan nama penyanyi: ");   // misal gini
+    STARTCOMMAND();                       // ini buat nerima command
+    printf("ini currentWord sekarang: "); // ini buat ngeprint command yang udah diambil
+    printWord(currentWord);               // ini buat ngeprint command yang udah diambil
+    WordToString(currentWord, penyanyi);  // ini buat convert word ke string
+    printf("ini penyanyi sekarang: ");    // ini buat ngeprint string yang udah diambil
+    printf("%s", penyanyi);               // ini buat ngeprint string yang udah diambil
 
     boolean exitRule = true;
 
