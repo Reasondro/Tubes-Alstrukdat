82c8de77fc597d6db8d75352549d653469a52eb3
Sitanggang Jerry Josia Partogi <79432841+Zeree17@users.noreply.github.com>
2023-11-13 10:54:29 +0700

    Update ADT List
    coba dicek lagi @Reasondro

---
+++ src/ADT/List/list.c
@@ -0,0 +1,50 @@
+// list.c
+
+#include <stdio.h>
+#include "list.h"
+
+// Implementasi fungsi untuk menampilkan daftar penyanyi
+void showSingerList(PenyanyiType penyanyiList[], int jumlahPenyanyi) {
+    printf("Daftar Penyanyi :\n");
+    for (int i = 0; i < jumlahPenyanyi; i++) {
+        printf("%s\n", penyanyiList[i].penyanyi);
+    }
+}
+
+// Implementasi fungsi untuk menampilkan daftar album dari penyanyi tertentu
+void showAlbumList(PenyanyiType penyanyiList[], int jumlahPenyanyi, char penyanyi[]) {
+    for (int i = 0; i < jumlahPenyanyi; i++) {
+        if (strcmp(penyanyiList[i].penyanyi, penyanyi) == 0) {
+            printf("Daftar Album oleh %s :\n", penyanyi);
+            for (int j = 0; j < jumlahAlbum; j++) {
+                printf("%s\n", penyanyiList[i].album[j].key);
+            }
+            break;
+        }
+    }
+}
+
+// Implementasi fungsi untuk menampilkan daftar lagu dari album tertentu
+void showSongList(AlbumType albumList[], int jumlahAlbum, char album[]) {
+    for (int i = 0; i < jumlahAlbum; i++) {
+        if (strcmp(albumList[i].key, album) == 0) {
+            printf("Daftar Lagu di %s:\n", album);
+            for (int j = 0; j < jumlahLagu; j++) {
+                printf("%s\n", albumList[i].song[j].judul);
+            }
+            break;
+        }
+    }
+}
+
+// Implementasi fungsi untuk menampilkan daftar playlist yang dimiliki pengguna
+void showPlaylistList(PlaylistType playlistList[], int jumlahPlaylist) {
+    if (jumlahPlaylist > 0) {
+        printf("Daftar playlist yang kamu miliki:\n");
+        for (int i = 0; i < jumlahPlaylist; i++) {
+            printf("%s\n", playlistList[i].namaPlaylist);
+        }
+    } else {
+        printf("Kamu tidak memiliki playlist.\n");
+    }
+}
---
+++ src/ADT/List/list.h
@@ -0,0 +1,20 @@
+// list.h
+
+#ifndef LIST_H
+#define LIST_H
+
+#include "struct.h"
+
+// Fungsi untuk menampilkan daftar penyanyi
+void showSingerList(PenyanyiType penyanyiList[], int jumlahPenyanyi);
+
+// Fungsi untuk menampilkan daftar album dari penyanyi tertentu
+void showAlbumList(PenyanyiType penyanyiList[], int jumlahPenyanyi, char penyanyi[]);
+
+// Fungsi untuk menampilkan daftar lagu dari album tertentu
+void showSongList(AlbumType albumList[], int jumlahAlbum, char album[]);
+
+// Fungsi untuk menampilkan daftar playlist yang dimiliki pengguna
+void showPlaylistList(PlaylistType playlistList[], int jumlahPlaylist);
+
+#endif
